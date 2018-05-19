876593e290a181246163ceb77b7fcf6812035694
lajeff77 <lajeff77@gmail.com>
2018-05-19 16:28:54 -0700

    Thread added This version was updated to include application as a thread of its own.

--- Clock/src/main/Application.java
+++ Clock/src/main/Application.java
@@ -4,23 +4,25 @@
  * manages all aspects of interaction and contains the thread for the
  * application.</p>
  * 
- * Created:5/19/18
+ * <p>Created:5/19/18</p>
  * @version 5/19/18
  * 
  * @author Lauryn Jefferson
  */
 package main;
 
-public class Application
+public class Application implements Runnable
 {
 	//constants
 	private static final int DEFAULT_WIDTH = 800;
 	private static final int DEFAULT_HEIGHT = 800;
 	
 	//objects
+	private Thread thread;
 	
 	//variables
-	private int width, height;
+	private int width, height;//for window dimensions
+	boolean running;//whether its running or not
 	
 	/**
 	 * <h2>Application Constructor</h2>
@@ -48,5 +50,139 @@ public class Application
 		//set window width and height
 		this.width = width;
 		this.height = height;
+		
+		//set conditions
+		running = false;
+	}
+	
+	public void run()
+	{
+		init();
+		int fps = 60;
+		double updateTime = 1000000000 / fps;
+		double change = 0;
+		long currentTime;
+		long beginningTime = System.nanoTime();
+		
+		while(running)
+		{
+			currentTime = System.nanoTime();
+			change += (currentTime - beginningTime) / updateTime;
+			beginningTime = currentTime;
+			
+			if(change >= 1)
+			{
+				update();
+				render();
+				change--;
+			}
+		}
+		
+	}
+	
+	/**
+	 * <h2>init() method</h2>
+	 * 
+	 * <p>This method initializes the necessary managers
+	 * and listeners for the Application to function properly.</p>
+	 */
+	private void init()
+	{
+		//Initialize stuff
+	}
+	
+	/**
+	 * <h2>start() method</h2>
+	 * 
+	 * <p>This method creates and starts the thread 
+	 * for the application.</p>
+	 */
+	public void start() throws AlreadyRunningException
+	{
+		//check if its running already
+		if(running)
+			throw new AlreadyRunningException();
+		
+		//setup if not running
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	/**
+	 * <h2>stop() method</h2>
+	 * 
+	 * <p>This method properly shuts down the thread
+	 * for the application.</p>
+	 */
+	public void stop()
+	{
+		running = false;
+	}
+	
+	/**
+	 * <h2>update() method</h2>
+	 * 
+	 * <p>This method maintains all of the elements
+	 * of the Application and keeps them up to speed
+	 * with the cycle of the Application.</p>
+	 */
+	private void update()
+	{
+		//update stuff
+	}
+	
+	/**
+	 * <h2>render() method</h2>
+	 * 
+	 * <p>This method renders all of the graphical
+	 * elements of the application to the screen after
+	 * they've been updated.</p>
+	 */
+	private void render()
+	{
+		//render stuff
+	}
+	
+	/**
+	 * <h1>AlreadyRunningException class</h1>
+	 * 
+	 * <p>An exception that deals with when the user attempts 
+	 * to run the thread when its already running.</p>
+	 * 
+	 * <p>Created:5/19/18</p>
+	 * @version 5/19/19
+	 * 
+	 * @author Lauryn Jefferson
+	 */
+	protected class AlreadyRunningException extends Exception
+	{
+		//because Exceptions are Serializable
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * <h2>AlreadyRunningException() constructor</h2>
+		 * 
+		 * <p>This default constructor uses "The Application 
+		 * thread is already running." as the error message
+		 * for the Exception.</p>
+		 */
+		public AlreadyRunningException()
+		{
+			this("The Application thread is already running.");
+		}
+		
+		/**
+		 * <h2>AlreadyRunningException() constructor</h2>
+		 * 
+		 * <p>This constructor creates the Exception and sets 
+		 * it's method.</p>
+		 * 
+		 * @param message the error message that displays
+		 */
+		public AlreadyRunningException(String message)
+	    {
+	        super(message);
+	    }
 	}
 }
--- Clock/src/main/Launcher.java
+++ Clock/src/main/Launcher.java
@@ -1,23 +1,43 @@
 /**
  * <h1>Launcher Class</h1>
  * 
- * <p></p>
+ * <p>This class is the launching point for the whole project.</p>
  * 
- * @param args 
- * 
- * Created:5/19/18
+ * <p>Created:5/19/18</p>
  * @version 5/19/18
  * 
  * @author Lauryn Jefferson
  */
 package main;
 
+import main.Application.AlreadyRunningException;
+
 public class Launcher 
 {
+	/**
+	 * <h2>main() method</h2>
+	 * 
+	 * <p>This method is the starting point that the JVM
+	 * recognizes when running this program.</p>
+	 * 
+	 * @param args arguments for the starting conditions of the program
+	 */
 	public static void main(String[] args)
 	{
 		//sets up application with window size of 1920 by 1080
 		Application clockApp = new Application(1920,1080);
 		
+		
+		//hands over the duties to the clock application thread
+		try
+		{
+			//try to start up the clock application
+			clockApp.start();
+		}
+		catch(AlreadyRunningException e)
+		{
+			//error in starting up the app
+			e.printStackTrace();
+		}
 	}
 }
